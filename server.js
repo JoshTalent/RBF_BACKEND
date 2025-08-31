087d006aed867681b86c29081794abb6e33176cd
joshTalent <ntwarijosue5@gmail.com>
2025-08-31 16:47:19 +0300

    connect to mongo atlas

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import path from "path";
-
-const app = express()
-
+import dotenv from "dotenv";
+const app = express();
 
 // file config
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 // routes
 
@@ -22,30 +20,36 @@ import eventsRoute from "./routes/eventsRoute.js"; // ✅ correct ES import
 import postRoute from "./routes/postRoute.js"; // ✅ correct ES import
 import matchesRoute from "./routes/matchesRoute.js"; // ✅ correct ES import
 
-
 // middlewares
+dotenv.config();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // database connection
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('✅ DB connected successfully'))
-  .catch((err) => console.error('❌ DB connection error:', err));
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      ssl: true,                     // Force SSL
+      tlsAllowInvalidCertificates: true, // Helps on Windows local dev
+    });
+    console.log("✅ MongoDB connected dghrgjdkfnjksbvhbs hrt");
+  } catch (err) {
+    console.error("❌ DB connection error:", err);
+  }
+};
 
-
+connectDB()
 // route callings to server
-app.use("/admin" , adminRoute);
-app.use("/boxers" , boxersRoute);
-app.use("/news" , newsRoute);
-app.use("/post" , postRoute);
-app.use("/events" , eventsRoute);
-app.use("/matches" , matchesRoute);
-
-
+app.use("/admin", adminRoute);
+app.use("/boxers", boxersRoute);
+app.use("/news", newsRoute);
+app.use("/post", postRoute);
+app.use("/events", eventsRoute);
+app.use("/matches", matchesRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
